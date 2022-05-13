316758924e13e974e226e172d787a0e67bb35914
lvalle <Luis.valle@mercadolibre.com.co>
2022-05-13 14:25:03 -0500

    docker config

---
+++ src/test/java/co/com/lbvp/aws/localstack/LocalStackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.lbvp.aws.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
